a33dab74b282bf13479c62b8e3944393efad10b8
naudecle <naudecle@gmail.com>
2024-05-11 16:33:05 +0400

    Managing state and learnig the use of useState

--- TypeScript-project/src/components/ListGroup.tsx
+++ TypeScript-project/src/components/ListGroup.tsx
@@ -1,15 +1,24 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 
 function ListGroup() {
   let items = ["Benin", "Burundi", "Burkina Faso", "Botswana", "Brazaville"];
-  const handleClick = (event: MouseEvent) => console.log(event);
+  const [selectedIndex, setSlectedIndex] = useState(-1);
+
   return (
     <>
       <h1>List Group</h1>
       {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSlectedIndex(index)}
+          >
             {item}
           </li>
         ))}
