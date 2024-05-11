d4eff53ffd47a08fed3be4a51d8385790f017e8a
naudecle <naudecle@gmail.com>
2024-05-11 16:11:37 +0400

    rendering List and conditional rendering

--- TypeScript-project/src/components/ListGroup.tsx
+++ TypeScript-project/src/components/ListGroup.tsx
@@ -1,17 +1,17 @@
-import { Fragment } from "react";
-
 function ListGroup() {
+  let items = ["Benin", "Burundi", "Burkina Faso", "Botswana", "Brazaville"];
+  items = [];
+
   return (
-    <Fragment>
+    <>
       <h1>List Group</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
-    </Fragment>
+    </>
   );
 }
 
